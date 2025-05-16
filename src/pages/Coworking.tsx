166168e570a24c8356949be26e00c51333ff7a0c
LautyKeob <lautaro.suarez.keob@gmail.com>
2025-05-16 17:04:24 +0200

    Deploy application to Netlify

--- src/pages/Coworking.tsx
+++ src/pages/Coworking.tsx
@@ -250,7 +250,7 @@ const Coworking = () => {
             {/* Business Image */}
             <div className="h-[400px] rounded-lg overflow-hidden shadow-md">
               <img 
-                src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" 
+                src="https://i.imgur.com/ftDDlTN.jpeg" 
                 alt="Business Hosting" 
                 className="w-full h-full object-cover"
               />
