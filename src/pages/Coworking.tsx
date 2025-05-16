711cec96448a579d22d433ec631cd61a63eb38b8
LautyKeob <lautaro.suarez.keob@gmail.com>
2025-05-16 16:28:18 +0200

    Deploy application to Netlify

--- src/pages/Coworking.tsx
+++ src/pages/Coworking.tsx
@@ -102,8 +102,7 @@ const Coworking = () => {
             <div className="bg-white rounded-lg shadow-md overflow-hidden">
               <div className="h-48 relative overflow-hidden">
                 <img 
-                  src="https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" 
-                  alt="Monthly Subscriptions" 
+                  src="https://i.imgur.com/1HHaKiT.jpeg" 
                   className="w-full h-full object-cover transform transition-transform hover:scale-110"
                 />
               </div>
